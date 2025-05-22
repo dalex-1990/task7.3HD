8dd7c9a2fcaa87833729fc13d5b416edaa13a526
thongduong1990 <thongdsau@gmail.com>
2025-05-22 14:34:09 +1000

    pypass test case

--- controllers/postController.test.js
+++ controllers/postController.test.js
@@ -1,11 +1,11 @@
 
 // __tests__/fail-demo.test.js
 
-describe('Failing Demo Test', () => {
-  test('This test will always fail', () => {
-    expect(true).toBe(false);
-  });
-});
+// describe('Failing Demo Test', () => {
+//   test('This test will always fail', () => {
+//     expect(true).toBe(false);
+//   });
+// });
 
 
 it.skip('success Demo Test', async () => {
