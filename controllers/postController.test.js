03c7bd4a34e58c3dbae8bb20138003c45d9c5717
thongduong1990 <thongdsau@gmail.com>
2025-05-22 13:10:24 +1000

    fail

--- controllers/postController.test.js
+++ controllers/postController.test.js
@@ -1,4 +1,13 @@
 
-it.skip('should create a post with valid data', async () => {
+// __tests__/fail-demo.test.js
+
+describe('Failing Demo Test', () => {
+  test('This test will always fail', () => {
     expect(true).toBe(false);
+  });
+});
+
+
+it.skip('should create a post with valid data', async () => {
+    
 });
